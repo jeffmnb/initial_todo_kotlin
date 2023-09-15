4ec9674160f980007507169cb568bfcb7711c72d
Jeferson <jeferson.nborda@gmail.com>
2023-09-14 23:24:24 -0300

    initializing app

--- app/src/main/java/com/example/initialtodo/MainActivity.kt
+++ app/src/main/java/com/example/initialtodo/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.initialtodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.initialtodo.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+//    val listaDeTarefas: Array<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setAddButtonClick()
+    }
+
+    fun setAddButtonClick(){
+        binding.addButton.setOnClickListener() {
+            println("clicou!")
+        }
     }
 }
+
